2bd246339e4a2d36a6ce962882b2c75699aa230e
Di Wang <wayne.wangdi@yandex.com>
2015-10-16 20:36:54 +0800

    add some functions for PrettyPrinter

--- src/PrettyPrinter.java
+++ src/PrettyPrinter.java
@@ -7,9 +7,15 @@ import java.util.ArrayList;
  */
 public class PrettyPrinter {
 
+    static final FormatProgramV formatProgram = new FormatProgramV();
+    static final FormatStatementV formatStatement = new FormatStatementV();
+    static final FormatExpressionV formatExpression = new FormatExpressionV();
+    static final FormatSwitchCaseV formatSwitchCase = new FormatSwitchCaseV();
+    static final FormatCatchClauseV formatCatchClause = new FormatCatchClauseV();
+    static final FormatVariableDeclaratorV formatVariableDeclarator = new FormatVariableDeclaratorV();
+
     static class FormatProgramV implements ProgramVisitor {
         public Object forProgram(ArrayList<Statement> body) {
-            FormatStatementV formatStatement = new FormatStatementV();
             StringBuilder builder = new StringBuilder();
             for (Statement stmt : body) {
                 builder.append((String)stmt.accept(formatStatement));
@@ -20,7 +26,6 @@ public class PrettyPrinter {
 
     static class FormatStatementV implements StatementVisitor {
         public Object forBlockStatement(ArrayList<Statement> body) {
-            FormatStatementV formatStatement = new FormatStatementV();
             StringBuilder builder = new StringBuilder();
             builder.append("{\n");
             for (Statement stmt : body) {
@@ -54,11 +59,9 @@ public class PrettyPrinter {
             return builder.toString();
         }
         public Object forDebuggerStatement() {
-
+            return "debug();\n";
         }
         public Object forDoWhileStatement(Statement body, Expression test) {
-            FormatStatementV formatStatement = new FormatStatementV();
-            FormatExpressionV formatExpression = new FormatExpressionV();
             StringBuilder builder = new StringBuilder();
             builder.append("do\n");
             builder.append((String)body.accept(formatStatement));
@@ -71,12 +74,9 @@ public class PrettyPrinter {
             return ";\n";
         }
         public Object forExpressionStatement(Expression expression) {
-            FormatExpressionV formatExpression = new FormatExpressionV();
             return expression.accept(formatExpression);
         }
         public Object forForInStatement(Node left, Expression right, Statement body) {
-            FormatExpressionV formatExpression = new FormatExpressionV();
-            FormatStatementV formatStatement = new FormatStatementV();
             StringBuilder builder = new StringBuilder();
             builder.append("for (");
             if (left instanceof VariableDeclaration) {
@@ -91,8 +91,6 @@ public class PrettyPrinter {
             return builder.toString();
         }
         public Object forForStatement(Node init, Expression test, Expression update, Statement body) {
-            FormatExpressionV formatExpression = new FormatExpressionV();
-            FormatStatementV formatStatement = new FormatStatementV();
             StringBuilder builder = new StringBuilder();
             builder.append("for (");
             if (init instanceof VariableDeclaration) {
@@ -109,7 +107,6 @@ public class PrettyPrinter {
             return builder.toString();
         }
         public Object forFunctionDeclaration(String id, ArrayList<String> params, BlockStatement body) {
-            FormatStatementV formatStatement = new FormatStatementV();
             StringBuilder builder = new StringBuilder();
             builder.append("function ");
             builder.append(id);
@@ -127,18 +124,140 @@ public class PrettyPrinter {
             builder.append((String)body.accept(formatStatement));
             return builder.toString();
         }
-        Object forIfStatement(Expression test, Statement consequent, Statement alternate);
-        Object forLabeledStatement(String label, Statement body);
-        Object forReturnStatement(Expression argument);
-        Object forSwitchStatement(Expression discriminant, ArrayList<SwitchCase> cases);
-        Object forThrowStatement(Expression argument);
-        Object forTryStatement(BlockStatement block, CatchClause handler, BlockStatement finalizer);
-        Object forVariableDeclaration(ArrayList<VariableDeclarator> declarations);
-        Object forWhileStatement(Expression test, Statement body);
-        Object forWithStatement(Expression object, Statement body);
+        public Object forIfStatement(Expression test, Statement consequent, Statement alternate) {
+            StringBuilder builder = new StringBuilder();
+            builder.append("if (");
+            builder.append((String)test.accept(formatExpression));
+            builder.append(")\n");
+            builder.append((String)consequent.accept(formatStatement));
+            if (alternate != null) {
+                builder.append("else\n");
+                builder.append((String)alternate.accept(formatStatement));
+            }
+            return builder.toString();
+        }
+        public Object forLabeledStatement(String label, Statement body) {
+            StringBuilder builder = new StringBuilder();
+            builder.append(label);
+            builder.append(":\n");
+            builder.append((String)body.accept(formatStatement));
+            return builder.toString();
+        }
+        public Object forReturnStatement(Expression argument) {
+            StringBuilder builder = new StringBuilder();
+            builder.append("return");
+            if (argument == null) {
+                builder.append(";\n");
+            } else {
+                builder.append(" ");
+                builder.append((String)argument.accept(formatExpression));
+                builder.append(";\n");
+            }
+            return builder.toString();
+        }
+        public Object forSwitchStatement(Expression discriminant, ArrayList<SwitchCase> cases) {
+            StringBuilder builder = new StringBuilder();
+            builder.append("switch (");
+            builder.append((String)discriminant.accept(formatExpression));
+            builder.append(")\n{\n");
+            for (SwitchCase sc : cases) {
+                builder.append((String)sc.accept(formatSwitchCase));
+            }
+            builder.append("}\n");
+            return builder.toString();
+        }
+        public Object forThrowStatement(Expression argument) {
+            StringBuilder builder = new StringBuilder();
+            builder.append("throw ");
+            builder.append((String)argument.accept(formatExpression));
+            builder.append(";\n");
+            return builder.toString();
+        }
+        public Object forTryStatement(BlockStatement block, CatchClause handler, BlockStatement finalizer) {
+            StringBuilder builder = new StringBuilder();
+            builder.append("try\n");
+            builder.append((String)block.accept(formatStatement));
+            builder.append((String)handler.accept(formatCatchClause));
+            builder.append("\nfinal\n");
+            builder.append((String)finalizer.accept(formatStatement));
+            return block.toString();
+        }
+        public Object forVariableDeclaration(ArrayList<VariableDeclarator> declarations) {
+            StringBuilder builder = new StringBuilder();
+            builder.append("var ");
+            boolean first = true;
+            for (VariableDeclarator decl : declarations) {
+                if (first) {
+                    first = false;
+                } else {
+                    builder.append(", ");
+                }
+                builder.append((String)decl.accept(formatVariableDeclarator));
+            }
+            builder.append(";\n");
+            return builder.toString();
+        }
+        public Object forWhileStatement(Expression test, Statement body) {
+            StringBuilder builder = new StringBuilder();
+            builder.append("while (");
+            builder.append((String)test.accept(formatExpression));
+            builder.append(")\n");
+            builder.append((String)body.accept(formatStatement));
+            return builder.toString();
+        }
+        public Object forWithStatement(Expression object, Statement body) {
+            StringBuilder builder = new StringBuilder();
+            builder.append("with ");
+            builder.append((String)object.accept(formatExpression));
+            builder.append("\n");
+            builder.append((String)body.accept(formatStatement));
+            return builder.toString();
+        }
     }
 
     static class FormatExpressionV implements ExpressionVisitor {
+        @Override
+        public Object forArrayExpression(ArrayList<Expression> elements) {
+            return null;
+        }
+    }
 
+    static class FormatSwitchCaseV implements SwitchCaseVisitor {
+        @Override
+        public Object forSwitchCase(Expression test, ArrayList<Statement> consequent) {
+            StringBuilder builder = new StringBuilder();
+            builder.append("case ");
+            builder.append((String)test.accept(formatExpression));
+            builder.append(":\n");
+            for (Statement stmt : consequent) {
+                builder.append((String)stmt.accept(formatStatement));
+            }
+            return builder.toString();
+        }
+    }
+
+    static class FormatCatchClauseV implements CatchClauseVisitor {
+        @Override
+        public Object forCatchClause(String param, BlockStatement body) {
+            StringBuilder builder = new StringBuilder();
+            builder.append("catch (");
+            builder.append(param);
+            builder.append(")\n");
+            builder.append((String)body.accept(formatStatement));
+            return builder.toString();
+        }
+    }
+
+    static class FormatVariableDeclaratorV implements VariableDeclaratorVisitor {
+        @Override
+        public Object forVariableDeclarator(String id, Expression init) {
+            StringBuilder builder = new StringBuilder();
+            builder.append(id);
+            if (init != null) {
+                builder.append(" = ");
+                builder.append((String)init.accept(formatExpression));
+            }
+            return builder.toString();
+        }
     }
 }
