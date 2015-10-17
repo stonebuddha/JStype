d5d64fb3ae38136351e41fdd86228db7701e80a6
Di Wang <wayne.wangdi@yandex.com>
2015-10-17 11:29:06 +0800

    complete PrettyPrinter

--- src/PrettyPrinter.java
+++ src/PrettyPrinter.java
@@ -10,15 +10,18 @@ public class PrettyPrinter {
     static final FormatProgramV formatProgram = new FormatProgramV();
     static final FormatStatementV formatStatement = new FormatStatementV();
     static final FormatExpressionV formatExpression = new FormatExpressionV();
+    static final FormatLiteralV formatLiteral = new FormatLiteralV();
     static final FormatSwitchCaseV formatSwitchCase = new FormatSwitchCaseV();
     static final FormatCatchClauseV formatCatchClause = new FormatCatchClauseV();
     static final FormatVariableDeclaratorV formatVariableDeclarator = new FormatVariableDeclaratorV();
+    static final FormatPropertyV formatProperty = new FormatPropertyV();
 
     static class FormatProgramV implements ProgramVisitor {
         public Object forProgram(ArrayList<Statement> body) {
             StringBuilder builder = new StringBuilder();
             for (Statement stmt : body) {
                 builder.append((String)stmt.accept(formatStatement));
+                builder.append("\n");
             }
             return builder.toString();
         }
@@ -30,19 +33,20 @@ public class PrettyPrinter {
             builder.append("{\n");
             for (Statement stmt : body) {
                 builder.append((String)stmt.accept(formatStatement));
+                builder.append("\n");
             }
-            builder.append("}\n");
+            builder.append("}");
             return builder.toString();
         }
         public Object forBreakStatement(String label) {
             StringBuilder builder = new StringBuilder();
             builder.append("break");
             if (label == null) {
-                builder.append(";\n");
+                builder.append(";");
             } else {
                 builder.append(" ");
                 builder.append(label);
-                builder.append(";\n");
+                builder.append(";");
             }
             return builder.toString();
         }
@@ -50,31 +54,34 @@ public class PrettyPrinter {
             StringBuilder builder = new StringBuilder();
             builder.append("continue");
             if (label == null) {
-                builder.append(";\n");
+                builder.append(";");
             } else {
                 builder.append(" ");
                 builder.append(label);
-                builder.append(";\n");
+                builder.append(";");
             }
             return builder.toString();
         }
         public Object forDebuggerStatement() {
-            return "debug();\n";
+            return "debug();";
         }
         public Object forDoWhileStatement(Statement body, Expression test) {
             StringBuilder builder = new StringBuilder();
-            builder.append("do\n");
+            builder.append("do ");
             builder.append((String)body.accept(formatStatement));
-            builder.append("while (");
+            builder.append(" while (");
             builder.append((String)test.accept(formatExpression));
-            builder.append(");\n");
+            builder.append(");");
             return builder.toString();
         }
         public Object forEmptyStatement() {
-            return ";\n";
+            return ";";
         }
         public Object forExpressionStatement(Expression expression) {
-            return expression.accept(formatExpression);
+            StringBuilder builder = new StringBuilder();
+            builder.append((String)expression.accept(formatExpression));
+            builder.append(";");
+            return builder.toString();
         }
         public Object forForInStatement(Node left, Expression right, Statement body) {
             StringBuilder builder = new StringBuilder();
@@ -86,7 +93,7 @@ public class PrettyPrinter {
             }
             builder.append(" in ");
             builder.append((String)right.accept(formatExpression));
-            builder.append(")\n");
+            builder.append(") ");
             builder.append((String)body.accept(formatStatement));
             return builder.toString();
         }
@@ -102,7 +109,7 @@ public class PrettyPrinter {
             builder.append((String)test.accept(formatExpression));
             builder.append("; ");
             builder.append((String)update.accept(formatExpression));
-            builder.append(")\n");
+            builder.append(") ");
             builder.append((String)body.accept(formatStatement));
             return builder.toString();
         }
@@ -120,7 +127,7 @@ public class PrettyPrinter {
                 }
                 builder.append(param);
             }
-            builder.append(")\n");
+            builder.append(") ");
             builder.append((String)body.accept(formatStatement));
             return builder.toString();
         }
@@ -128,10 +135,10 @@ public class PrettyPrinter {
             StringBuilder builder = new StringBuilder();
             builder.append("if (");
             builder.append((String)test.accept(formatExpression));
-            builder.append(")\n");
+            builder.append(") ");
             builder.append((String)consequent.accept(formatStatement));
             if (alternate != null) {
-                builder.append("else\n");
+                builder.append(" else ");
                 builder.append((String)alternate.accept(formatStatement));
             }
             return builder.toString();
@@ -147,11 +154,11 @@ public class PrettyPrinter {
             StringBuilder builder = new StringBuilder();
             builder.append("return");
             if (argument == null) {
-                builder.append(";\n");
+                builder.append(";");
             } else {
                 builder.append(" ");
                 builder.append((String)argument.accept(formatExpression));
-                builder.append(";\n");
+                builder.append(";");
             }
             return builder.toString();
         }
@@ -159,26 +166,26 @@ public class PrettyPrinter {
             StringBuilder builder = new StringBuilder();
             builder.append("switch (");
             builder.append((String)discriminant.accept(formatExpression));
-            builder.append(")\n{\n");
+            builder.append(") {\n");
             for (SwitchCase sc : cases) {
                 builder.append((String)sc.accept(formatSwitchCase));
             }
-            builder.append("}\n");
+            builder.append("}");
             return builder.toString();
         }
         public Object forThrowStatement(Expression argument) {
             StringBuilder builder = new StringBuilder();
             builder.append("throw ");
             builder.append((String)argument.accept(formatExpression));
-            builder.append(";\n");
+            builder.append(";");
             return builder.toString();
         }
         public Object forTryStatement(BlockStatement block, CatchClause handler, BlockStatement finalizer) {
             StringBuilder builder = new StringBuilder();
-            builder.append("try\n");
+            builder.append("try ");
             builder.append((String)block.accept(formatStatement));
             builder.append((String)handler.accept(formatCatchClause));
-            builder.append("\nfinal\n");
+            builder.append(" final ");
             builder.append((String)finalizer.accept(formatStatement));
             return block.toString();
         }
@@ -194,14 +201,14 @@ public class PrettyPrinter {
                 }
                 builder.append((String)decl.accept(formatVariableDeclarator));
             }
-            builder.append(";\n");
+            builder.append(";");
             return builder.toString();
         }
         public Object forWhileStatement(Expression test, Statement body) {
             StringBuilder builder = new StringBuilder();
             builder.append("while (");
             builder.append((String)test.accept(formatExpression));
-            builder.append(")\n");
+            builder.append(") ");
             builder.append((String)body.accept(formatStatement));
             return builder.toString();
         }
@@ -209,7 +216,7 @@ public class PrettyPrinter {
             StringBuilder builder = new StringBuilder();
             builder.append("with ");
             builder.append((String)object.accept(formatExpression));
-            builder.append("\n");
+            builder.append(" ");
             builder.append((String)body.accept(formatStatement));
             return builder.toString();
         }
@@ -218,7 +225,242 @@ public class PrettyPrinter {
     static class FormatExpressionV implements ExpressionVisitor {
         @Override
         public Object forArrayExpression(ArrayList<Expression> elements) {
-            return null;
+            StringBuilder builder = new StringBuilder();
+            builder.append("[");
+            boolean first = true;
+            for (Expression exp : elements) {
+                if (first) {
+                    first = false;
+                } else {
+                    builder.append(", ");
+                }
+                if (exp != null) {
+                    builder.append((String)exp.accept(formatExpression));
+                }
+            }
+            builder.append("]");
+            return builder.toString();
+        }
+
+        @Override
+        public Object forAssignmentExpression(String operator, Object left, Expression right) {
+            StringBuilder builder = new StringBuilder();
+            if (left instanceof String) {
+                builder.append(left);
+            } else {
+                builder.append((String)((Expression)left).accept(formatExpression));
+            }
+            builder.append(" ");
+            builder.append(operator);
+            builder.append(" ");
+            builder.append((String)right.accept(formatExpression));
+            return builder.toString();
+        }
+
+        @Override
+        public Object forBinaryExpression(String operator, Expression left, Expression right) {
+            StringBuilder builder = new StringBuilder();
+            builder.append((String)left.accept(formatExpression));
+            builder.append(" ");
+            builder.append(operator);
+            builder.append(" ");
+            builder.append((String)right.accept(formatExpression));
+            return builder.toString();
+        }
+
+        @Override
+        public Object forCallExpression(Expression callee, ArrayList<Expression> arguments) {
+            StringBuilder builder = new StringBuilder();
+            builder.append((String)callee.accept(formatExpression));
+            builder.append("(");
+            boolean first = true;
+            for (Expression exp : arguments) {
+                if (first) {
+                    first = false;
+                } else {
+                    builder.append(", ");
+                }
+                builder.append((String)exp.accept(formatExpression));
+            }
+            builder.append(")");
+            return builder.toString();
+        }
+
+        @Override
+        public Object forConditionalExpression(Expression test, Expression alternate, Expression consequent) {
+            StringBuilder builder = new StringBuilder();
+            builder.append((String)test.accept(formatExpression));
+            builder.append(" ? ");
+            builder.append((String)consequent.accept(formatExpression));
+            builder.append(" : ");
+            builder.append((String)alternate.accept(formatExpression));
+            return builder.toString();
+        }
+
+        @Override
+        public Object forFunctionExpression(String id, ArrayList<String> params, BlockStatement body) {
+            StringBuilder builder = new StringBuilder();
+            builder.append("function ");
+            if (id != null) {
+                builder.append(id);
+            }
+            builder.append("(");
+            boolean first = true;
+            for (String p : params) {
+                if (first) {
+                    first = false;
+                } else {
+                    builder.append(", ");
+                }
+                builder.append(p);
+            }
+            builder.append(") ");
+            builder.append((String)body.accept(formatStatement));
+            return builder.toString();
+        }
+
+        @Override
+        public Object forIdentifierExpression(String name) {
+            return name;
+        }
+
+        @Override
+        public Object forLiteralExpression(Literal literal) {
+            return literal.accept(formatLiteral);
+        }
+
+        @Override
+        public Object forLogicalExpression(String operator, Expression left, Expression right) {
+            StringBuilder builder = new StringBuilder();
+            builder.append((String)left.accept(formatExpression));
+            builder.append(" ");
+            builder.append(operator);
+            builder.append(" ");
+            builder.append((String)right.accept(formatExpression));
+            return builder.toString();
+        }
+
+        @Override
+        public Object forMemberExpression(Expression object, Expression property, boolean computed) {
+            StringBuilder builder = new StringBuilder();
+            builder.append((String)object.accept(formatExpression));
+            builder.append("[");
+            builder.append((String)property.accept(formatExpression));
+            builder.append("]");
+            return builder.toString();
+        }
+
+        @Override
+        public Object forNewExpression(Expression callee, ArrayList<Expression> arguments) {
+            StringBuilder builder = new StringBuilder();
+            builder.append("new ");
+            builder.append((String)callee.accept(formatExpression));
+            builder.append("(");
+            boolean first = true;
+            for (Expression exp : arguments) {
+                if (first) {
+                    first = false;
+                } else {
+                    builder.append(", ");
+                }
+                builder.append((String)exp.accept(formatExpression));
+            }
+            builder.append(")");
+            return builder.toString();
+        }
+
+        @Override
+        public Object forObjectExpression(ArrayList<Property> properties) {
+            StringBuilder builder = new StringBuilder();
+            builder.append("{");
+            boolean first = true;
+            for (Property p : properties) {
+                if (first) {
+                    first = false;
+                } else {
+                    builder.append(", ");
+                }
+                builder.append((String)p.accept(formatProperty));
+            }
+            builder.append("}");
+            return builder.toString();
+        }
+
+        @Override
+        public Object forSequenceExpression(ArrayList<Expression> expressions) {
+            StringBuilder builder = new StringBuilder();
+            boolean first = true;
+            for (Expression exp : expressions) {
+                if (first) {
+                    first = false;
+                } else {
+                    builder.append(", ");
+                }
+                builder.append((String)exp.accept(formatExpression));
+            }
+            return builder.toString();
+        }
+
+        @Override
+        public Object forThisExpression() {
+            return "this";
+        }
+
+        @Override
+        public Object forUnaryExpression(String operator, boolean prefix, Expression argument) {
+            StringBuilder builder = new StringBuilder();
+            if (prefix) {
+                builder.append(operator);
+                builder.append((String)argument.accept(formatExpression));
+            } else {
+                builder.append((String)argument.accept(formatExpression));
+                builder.append(operator);
+            }
+            return builder.toString();
+        }
+
+        @Override
+        public Object forUpdateExpression(String operator, Expression argument, boolean prefix) {
+            StringBuilder builder = new StringBuilder();
+            if (prefix) {
+                builder.append(operator);
+                builder.append((String)argument.accept(formatExpression));
+            } else {
+                builder.append((String)argument.accept(formatExpression));
+                builder.append(operator);
+            }
+            return builder.toString();
+        }
+    }
+
+    static class FormatLiteralV implements LiteralVisitor {
+        @Override
+        public Object forBooleanLiteral(boolean value) {
+            if (value) {
+                return "true";
+            } else {
+                return "false";
+            }
+        }
+
+        @Override
+        public Object forNullLiteral() {
+            return "null";
+        }
+
+        @Override
+        public Object forNumberLiteral(Number value) {
+            return value.toString();
+        }
+
+        @Override
+        public Object forRegExpLiteral(String pattern, String flags) {
+            return "/" + pattern + "/" + flags + "/";
+        }
+
+        @Override
+        public Object forStringLiteral(String value) {
+            return "\"" + value + "\"";
         }
     }
 
@@ -242,7 +484,7 @@ public class PrettyPrinter {
             StringBuilder builder = new StringBuilder();
             builder.append("catch (");
             builder.append(param);
-            builder.append(")\n");
+            builder.append(") ");
             builder.append((String)body.accept(formatStatement));
             return builder.toString();
         }
@@ -258,6 +500,13 @@ public class PrettyPrinter {
                 builder.append((String)init.accept(formatExpression));
             }
             return builder.toString();
+        }
+    }
+
+    static class FormatPropertyV implements PropertyVisitor {
+        @Override
+        public Object forProperty(Object key, Expression value, String kind) {
+            return null;
         }
     }
 }
