d7cd9c9cd68b6880abd6420c2a913333b8086318
Hwhitetooth <hwhitetooth@gmail.com>
2015-11-05 17:14:56 +0800

    Added LetStatement and DebuggerStatement. Modified Parser.

--- src/concrete/Utils.java
+++ src/concrete/Utils.java
@@ -2,8 +2,10 @@ package concrete;
 
 import com.google.common.collect.ImmutableList;
 
-import ir.IRScratch;
-import ir.IRVar;
+import com.google.common.collect.ImmutableMap;
+import com.sun.tools.corba.se.idl.constExpr.Negative;
+import concrete.init.Init;
+import ir.*;
 
 import java.util.AbstractMap;
 import java.util.ArrayList;
@@ -17,6 +19,11 @@ public class Utils {
 
     public static class Recursive<I> {
         public I func;
+    }
+
+    public static class Errors {
+        public static final Domains.EValue typeError = new Domains.EValue(new Domains.Str("TypeError"));
+        public static final Domains.EValue rangeError = new Domains.EValue(new Domains.Str("RangeError"));
     }
 
     public static class Fields {
@@ -46,18 +53,81 @@ public class Utils {
     }
 
     public static Map.Entry<Domains.Store, Domains.Address> allocFun(Domains.Closure clo, Domains.BValue n, Domains.Store store) {
-        // TODO
-        return null;
+        Domains.Address a1 = Domains.Address.generate();
+        ImmutableMap.Builder<Domains.Str, Object> internBuilder = ImmutableMap.builder();
+        internBuilder.put(Fields.proto, Init.Function_prototype_Addr);
+        internBuilder.put(Fields.classname, JSClass.CFunction);
+        internBuilder.put(Fields.code, clo);
+        ImmutableMap<Domains.Str, Object> intern = internBuilder.build();
+        ImmutableMap.Builder<Domains.Str, Domains.BValue> externBuilder = ImmutableMap.builder();
+        externBuilder.put(Fields.length, n);
+        ImmutableMap<Domains.Str, Domains.BValue> extern = externBuilder.build();
+        return new AbstractMap.SimpleEntry<Domains.Store, Domains.Address>(store.putObj(a1, new Domains.Object(extern, intern)), a1);
     }
 
     public static Map.Entry<Domains.Store, Domains.Address> allocObj(Domains.Address a, Domains.Store store) {
-        // TODO
-        return null;
+        JSClass c = Init.classFromAddress(a);
+        Domains.Address a1 = Domains.Address.generate();
+        Domains.Address a2;
+        Domains.BValue tmp = store.getObj(a).apply(Fields.prototype);
+        if (tmp == null) {
+            a2 = Init.Object_prototype_Addr;
+        }
+        else {
+            a2 = (Domains.Address)tmp;
+        }
+        ImmutableMap.Builder<Domains.Str, java.lang.Object> internBuilder = ImmutableMap.builder();
+        internBuilder.put(Fields.proto, a2);
+        internBuilder.put(Fields.classname, c);
+        ImmutableMap<Domains.Str, java.lang.Object> intern = internBuilder.build();
+        ImmutableMap.Builder<Domains.Str, Domains.BValue> tmpBuilder = ImmutableMap.builder();
+        Domains.Store store1 = store.putObj(a1, new Domains.Object(tmpBuilder.build(), intern));
+        return new AbstractMap.SimpleEntry<Domains.Store, Domains.Address>(store1, a1);
     }
 
     public static Interpreter.State applyClo(Domains.BValue bv1, Domains.BValue bv2, Domains.BValue bv3, IRVar x, Domains.Env env, Domains.Store store, Domains.Scratchpad pad, Domains.KontStack ks) {
-        // TODO
-        return null;
+        if (bv1 instanceof Domains.Address && bv2 instanceof Domains.Address && bv3 instanceof Domains.Address) {
+            Domains.Address a1 = (Domains.Address)bv1;
+            Domains.Address a2 = (Domains.Address)bv2;
+            Domains.Address a3 = (Domains.Address)bv3;
+            Domains.Object o = store.getObj(a1);
+            Boolean isCtor = store.getObj(a3).calledAsCtor();
+
+            Domains.Closure tmp = o.getCode();
+            if (tmp == null) {
+                return new Interpreter.State(new Domains.ValueTerm(Errors.typeError), env, store, pad, ks);
+            }
+            else if (tmp instanceof Domains.Clo) {
+                Domains.Clo tmp2 = (Domains.Clo)tmp;
+                Domains.Env envc = tmp2.env;
+                IRMethod m = tmp2.m;
+                IRPVar self = m.self;
+                IRPVar args = m.args;
+                IRStmt s = m.s;
+                ArrayList<Domains.BValue> tmpList = new ArrayList<>();
+                tmpList.add(a2);
+                tmpList.add(a3);
+                Map.Entry<Domains.Store, ArrayList<Domains.Address>> tmp3 = alloc(store, tmpList);
+                Domains.Store store1 = tmp3.getKey();
+                ArrayList<Domains.Address> as = tmp3.getValue();
+//                ArrayList<IRPVar> tmp4 = new ArrayList<IRPVar>();
+//                tmp4.add(self);
+//                tmp4.add(args);
+                ImmutableList.Builder<Map.Entry<IRPVar, Domains.Address>> listBuilder = ImmutableList.builder();
+                listBuilder.add(new AbstractMap.SimpleImmutableEntry<IRPVar, Domains.Address>(self, as.get(0)));
+                listBuilder.add(new AbstractMap.SimpleImmutableEntry<IRPVar, Domains.Address>(args, as.get(1)));
+                ImmutableList<Map.Entry<IRPVar, Domains.Address>> list = listBuilder.build();
+                Domains.Env envc1 = env.extendAll(list);
+                return new Interpreter.State(new Domains.StmtTerm(s), envc1, store1, Domains.Scratchpad.apply(0), ks.push(new Domains.RetKont(x, env, isCtor, pad)));
+            }
+            else {
+                //TODO: how to shatter high ordered functions
+                return null;
+            }
+        }
+        else {
+            return new Interpreter.State(new Domains.ValueTerm(Errors.typeError), env, store, pad, ks);
+        }
     }
 
     public static Map.Entry<Domains.Value, Map.Entry<Domains.Store, Domains.Scratchpad>> delete(Domains.BValue bv1, Domains.BValue bv2, IRScratch x, Domains.Env env, Domains.Store store, Domains.Scratchpad pad) {
--- src/concrete/init/Init.java
+++ src/concrete/init/Init.java
@@ -9,6 +9,9 @@ import ir.IRPVar;
 import ir.IRStmt;
 import ir.JSClass;
 
+import java.lang.reflect.Array;
+import java.util.Date;
+
 /**
  * Created by wayne on 15/10/29.
  */
@@ -16,6 +19,17 @@ public class Init {
 
     public static final IRPVar window_Variable = new IRPVar(0);
 
+    public static final Domains.Address Array_Addr = Domains.Address.generate();
+    public static final Domains.Address String_Addr = Domains.Address.generate();
+    public static final Domains.Address Boolean_Addr = Domains.Address.generate();
+    public static final Domains.Address Number_Addr = Domains.Address.generate();
+    public static final Domains.Address Date_Addr = Domains.Address.generate();
+    public static final Domains.Address Error_Addr = Domains.Address.generate();
+    public static final Domains.Address RegExp_Addr = Domains.Address.generate();
+    public static final Domains.Address Arguments_Addr = Domains.Address.generate();
+    public static final Domains.Address Function_Addr = Domains.Address.generate();
+    public static final Domains.Address Function_prototype_Addr = Domains.Address.generate();
+    public static final Domains.Address Object_prototype_Addr = Domains.Address.generate();
     // TODO
 
     public static Interpreter.State initState(IRStmt s) {
@@ -38,5 +52,36 @@ public class Init {
 
     public static final ImmutableMap<JSClass, ImmutableSet<Domains.Str>> noupdate = null; // TODO
 
-    public static final ImmutableMap<Domains.Address, JSClass> classFromAddress = null; // TODO
+    public static final JSClass classFromAddress(Domains.Address a) {
+        if (a == Function_Addr) {
+            return JSClass.CFunction;
+        }
+        else if (a == Array_Addr) {
+            return JSClass.CArray;
+        }
+        else if (a == String_Addr) {
+            return JSClass.CString;
+        }
+        else if (a == Boolean_Addr) {
+            return JSClass.CBoolean;
+        }
+        else if (a == Number_Addr) {
+            return JSClass.CNumber;
+        }
+        else if (a == Date_Addr) {
+            return JSClass.CDate;
+        }
+        else if (a == Error_Addr) {
+            return JSClass.CError;
+        }
+        else if (a == RegExp_Addr) {
+            return JSClass.CRegExp;
+        }
+        else if (a == Arguments_Addr) {
+            return JSClass.CArguments;
+        }
+        else {
+            return JSClass.CObject;
+        }
+    }
 }
