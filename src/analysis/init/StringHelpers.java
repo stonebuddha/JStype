a7d533197aadecb9961870ce05728f0d4ce71756
Hwhitetooth <hwhitetooth@gmail.com>
2015-12-09 21:57:40 +0800

    Sorry, I forgot this file...

---
+++ src/analysis/init/StringHelpers.java
@@ -0,0 +1,16 @@
+package analysis.init;
+
+import analysis.Domains;
+import fj.data.List;
+import fj.data.Option;
+
+/**
+ * Created by Hwhitetooth on 15/12/9.
+ */
+//TODO
+public class StringHelpers {
+    //TODO
+    public static Domains.Object newArray(Domains.Num length, List<Domains.BValue> exactEntries, Option<Domains.BValue> summaryVal, Domains.Object origArray, Boolean update) {
+        return null;
+    }
+}
