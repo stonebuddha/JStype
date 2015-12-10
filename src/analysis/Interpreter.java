afe7c7de75b63b409cc6fc16093d4972d6739d1a
Ben Zhong <benzhong@pku.edu.cn>
2015-12-10 14:55:49 +0800

    implementing StackCFA trace..

--- src/analysis/Interpreter.java
+++ src/analysis/Interpreter.java
@@ -76,7 +76,8 @@ public class Interpreter {
         });
 
         // default: flow-sensitive context-insensitive
-        Trace initTrace = new Traces.FSCI(0);
+        //Trace initTrace = new Traces.FSCI(0);
+        Trace initTrace = Traces.StackCFA.apply(2000, 1000);
         Mutable.splitStates = false;
         IRStmt ast = readIR(args[0]);
         HashMap<Trace, State> memo = new HashMap<Trace, State>(Equal.anyEqual(), Hash.anyHash());
